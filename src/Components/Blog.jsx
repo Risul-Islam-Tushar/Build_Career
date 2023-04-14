dcb2bf3df7fb088603db1eaa18e027c9ce3d0846
Risul-Islam-Tushar <risultushar@gmail.com>
2023-04-14 21:14:58 +0600

    question answer added

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-            Question Answers:
+        <div className='ms-28 mr-20 mt-8 mb-20'>
+            Question Answer: <br />
+            1. Context API can be used when we need to pass data from a parent component to a deeply nested child component without having to pass the data through every component in between.
+            <br />
+            2.
+            React custom hooks are a way to create reusable pieces of logic that can be shared across multiple components in your React application.
+            <br />
+            3.
+            Useref is a popular Node.js package that helps automate HTML file concatenation and minification.
+            <br />
+            4.
+            useMemo is a React hook that allows you to memoize, or cache, the output of a function.
         </div>
     );
 };
