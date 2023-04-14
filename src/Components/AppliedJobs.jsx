e973bca4ed7539c6e9cddae178f9d76e263ee516
Risul-Islam-Tushar <risultushar@gmail.com>
2023-04-14 20:34:48 +0600

    applied job added

--- src/Components/AppliedJobs.jsx
+++ src/Components/AppliedJobs.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import SingleJob from './SingleJob';
 
 const AppliedJobs = () => {
+
+    const [values, setValues] = useState([])
+    const [filterValue, setFilterValue] = useState([])
+
+    useEffect(() => {
+        const storedData = JSON.parse(localStorage.getItem("orders"));
+        if (storedData) {
+            setValues(storedData)
+            setFilterValue(storedData)
+        }
+    }, [])
+
     return (
-        <div>
-            applied
+        <div className='ms-28 mr-20 mt-8 mb-20 '>
+            <h1 className='text-center text-4xl font-bold mt-20 mb-16 '>Applied Job</h1>
+            {
+                filterValue.length > 0 ?
+                    filterValue?.map((job) => <div className='ms-96'><SingleJob job={job}></SingleJob></div>) :
+                    <h1>No job Found</h1>
+            }
+
         </div>
     );
 };
